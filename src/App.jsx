a8effb05ffee1a196b8f5b09b51c7bca355c3f7a
unknown <faroow456788@gmail.com>
2023-12-05 10:11:26 -0800

    birthdays

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import Birthday from './birthday_reminder/Birthday'
+
+
+function App() {
+  
+
+  return (
+    <>
+      < Birthday/>
+    </>
+  )
+}
+
+export default App
---
+++ src/birthday_reminder/data.js
@@ -0,0 +1,52 @@
+export const data = [
+    {    
+    id:8,
+    name:'Dennis',
+    age:'28',
+    image:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg" ,
+    
+    
+    },
+    
+    
+    {
+     id:9,
+     name:'Theodore',
+     age: '26' ,
+     image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg' ,
+    
+    
+    
+    },
+    {
+        id:10,
+        name:'Barry',
+        age:'25',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg' ,
+    
+    },
+    
+    {
+        id:'11',
+        name:'Sean Walsh',
+        age:'30',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg' ,
+    
+    
+    
+    },
+     {
+        id:'12',
+        name:'Hester Hogan',
+        age:'32',
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    
+     }
+    
+    
+    
+    
+    
+    
+    ]
+    
